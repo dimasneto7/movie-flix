6c46d9336c9862cc01ca27b405888a52d56f2be7
dimasneto7 <dimas_fcn@outlook.com>
2023-07-02 17:54:29 -0300

    backgroundImage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -42,10 +42,13 @@ export default function Home() {
           return (
             <article key={movie.id}>
               <strong>{movie.title}</strong>
-              <img
-                src={`https:image.tmdb.org/t/p/original/${movie.poster_path}`}
+              <div
+                className="movieInfoImgHome"
+                style={{
+                  backgroundImage: `url(https:image.tmdb.org/t/p/original/${movie.poster_path})`,
+                }}
                 alt={movie.title}
-              />
+              ></div>
               <Link to={`/movie/${movie.id}`}>Acessar</Link>
             </article>
           )
--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -69,10 +69,13 @@ export default function Movie() {
   return (
     <div className="movie-info">
       <h1>{movie.title}</h1>
-      <img
-        src={`https:image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+      <div
+        className="movieInfoImg"
+        style={{
+          backgroundImage: `url(https:image.tmdb.org/t/p/original/${movie.backdrop_path})`,
+        }}
         alt={movie.title}
-      />
+      ></div>
       <h3>Sinopse</h3>
       <span>{movie.overview}</span>
 
